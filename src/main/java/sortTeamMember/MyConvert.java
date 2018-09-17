ecbd1759dcf1169704fb7112859075d50d3a43be
Lianqing/Bluto <lqqu@thoughtworks.com>
2018-09-17 19:40:49 +0800

    [xiaoqu][sortPerson] imply single name convert to person list

--- src/main/java/sortTeamMember/MyConvert.java
+++ src/main/java/sortTeamMember/MyConvert.java
@@ -1,5 +1,6 @@
 package sortTeamMember;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
@@ -8,5 +9,9 @@ import java.util.stream.Collectors;
 public class MyConvert {
     public List<String> convertNameToList(String name) {
         return Arrays.stream(name.split(",")).collect(Collectors.toList());
+    }
+
+    public List<Person> convertNamesToPerson(List<String> nameList) {
+        return nameList.stream().map(name -> new Person(name)).collect(Collectors.toList());
     }
 }
--- src/test/java/sortTeamMember/ConvertTest.java
+++ src/test/java/sortTeamMember/ConvertTest.java
@@ -4,25 +4,36 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertIterableEquals;
 
-public class ConvertTest  {
+public class ConvertTest {
     private MyConvert convert;
+    private String names;
 
     @BeforeEach
     void setUp() {
         convert = new MyConvert();
+        names = "huang lizhen,lan yixing,yang kaiguang,sun ming";
     }
 
     @Test
     void should_convert_one_name_to_name_list() {
-      assertEquals("huang lizhen",convert.convertNameToList("huang lizhen").get(0));
+        assertEquals("huang lizhen", convert.convertNameToList("huang lizhen").get(0));
     }
 
     @Test
     void should_convert_multiple_name_to_name_list() {
-        assertIterableEquals(Arrays.asList("huang lizhen","lan yixing","yang kaiguang","sun ming"), convert.convertNameToList("huang lizhen,lan yixing,yang kaiguang,sun ming"));
+        assertIterableEquals(Arrays.asList("huang lizhen", "lan yixing", "yang kaiguang", "sun ming"), convert.convertNameToList(names));
     }
+
+    @Test
+    void should_convert_one_name_to_person() {
+        List<String> nameList = convert.convertNameToList(names);
+        assertEquals(new Person("huang lizhen"), convert.convertNamesToPerson(nameList).get(0));
+    }
+
+
 }
